19bfc7900c0c2b211272cb7de3d7fdd5f2799b06
chema1998 <105527025+chema1998@users.noreply.github.com>
2024-05-19 22:30:23 -0500

    Revert "Merge branch 'main' into jose"
    This reverts commit 90364c47e7d0173980d6a084d35bc2d00c4b82ed, reversing
    changes made to d980220ad80fa1e835c7002dc299b8aaac10d60b.

--- projecto1/Assets/scripts/prueva/InteractivoOBJ.cs
+++ projecto1/Assets/scripts/prueva/InteractivoOBJ.cs
@@ -8,7 +8,4 @@ public class ObjInteractivo : MonoBehaviour
     {
        Destroy(gameObject);
     }
-
-
-    
 }
--- projecto1/Assets/scripts/tapaControllerButton.cs
+++ projecto1/Assets/scripts/tapaControllerButton.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class tapaControllerButton : MonoBehaviour
 {
+    // Start is called before the first frame update
     public GameObject buttonOff;
     public GameObject buttonOn;
     public GameObject tapaObject;
@@ -12,51 +13,58 @@ public class tapaControllerButton : MonoBehaviour
     public Transform openPositionTransform;
     public Transform closedPositionTransform;
 
+
+
     public bool tapaState = false;  
     
     void Start()
     {
-        Initialize();
-    }
-
-    public void Initialize()
-    {
-        if (tapaObject.GetComponent<BoxCollider>() == null)
-        {
-            tapaObject.AddComponent<BoxCollider>();
-        }
-
-        if (baseButtonObject.GetComponent<BoxCollider>() == null)
-        {
-            baseButtonObject.AddComponent<BoxCollider>();
-        }
+        tapaObject.AddComponent<BoxCollider>();
+        baseButtonObject.AddComponent<BoxCollider>();
 
         SetTapaState(tapaState);
     }
 
     void SetTapaState(bool s)
     {
-        tapaState = s; // Actualizar el estado de la tapa
-
         buttonOn.SetActive(s);
         buttonOff.SetActive(!s);
+       
 
         if (s)
         {
             tapaObject.transform.rotation = openPositionTransform.rotation;
+
         }
-        else
-        {
-            tapaObject.transform.rotation = closedPositionTransform.rotation;
-        }
+         else
+         {
+             tapaObject.transform.rotation = closedPositionTransform.rotation;
+
+         }
     }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            Opentapa();
+        }
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            Closedtapa();
+        }
+
+    }
+
+   
 
     public void Opentapa()
     {
         Debug.Log("tapa abierta");
         SetTapaState(true);
     }
-
     public void Closedtapa()
     {
         Debug.Log("tapa cerrada");
