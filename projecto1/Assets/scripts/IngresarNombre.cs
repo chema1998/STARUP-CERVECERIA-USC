00c4671ae447a16388bad902fad9395c3529b39b
jefferson077 <106373869+jefferson077@users.noreply.github.com>
2024-05-28 22:30:52 -0500

    Actualización ingresar nombre

--- projecto1/Assets/scripts/IngresarNombre.cs
+++ projecto1/Assets/scripts/IngresarNombre.cs
@@ -3,15 +3,30 @@ using UnityEngine.UI;
 
 public class IngresarNombre : MonoBehaviour
 {
-    // Renombrar el miembro a algo más descriptivo, por ejemplo, "IngresarNombrePanel"
     public GameObject IngresarNombrePanel;
     public GameObject SeleccionarPerfil;
     
+    // Método para abrir un panel específico
     public void Open(GameObject panel)
     {
+        IngresarNombrePanel.SetActive(false);
         SeleccionarPerfil.SetActive(false);
 
-        // Usar el nuevo nombre del miembro
+        panel.SetActive(true);
+    }
+
+    // Método para pasar del panel IngresarNombre al panel SeleccionarPerfil
+    public void OnAcceptButtonClick()
+    {
+        IngresarNombrePanel.SetActive(false);
+        SeleccionarPerfil.SetActive(true);
+    }
+
+    // Método para pasar del panel SeleccionarPerfil al panel IngresarNombre
+    public void ShowIngresarNombrePanel()
+    {
+        SeleccionarPerfil.SetActive(false);
         IngresarNombrePanel.SetActive(true);
     }
 }
+
